5e0c76e6c369b0600e70edf17312d53005637e2a
Holland Pleskac <hollandpleskac@gmail.com>
2021-05-29 11:54:05 -0700

    login/signup/homepage

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,65 @@
 import React from 'react'
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const HomePage = () => {
+  return (
+    <div className='w-full h-screen flex flex-col ' >
+      <Header />
+      <HomeContent />
+    </div>
+  )
+}
 
-  const function1 = async () => {
-    console.log('adding to firebase')
-    await firebase.firestore().collection('test').add({
-      test: 'testing'
-    })
+const Header = () => {
+  const router = useRouter()
+  const navHandler = (params) => {
+    router.push('/login')
   }
 
 
   return (
-    <div onClick={function1} >
-      Home
+    <div className='w-full flex items-center justify-between shadow py-4 px-6' >
+      <div className='flex items-center' >
+        <img src="/logo-blue.png" width='55' alt="Logo" className='mr-3' />
+        <p className='text-xl text-gray-800' >Communify</p>
+      </div >
+      <div className='flex items-center' >
+        <LandingLink name='Home' href='#' />
+        <LandingLink name='About' href='#' />
+        <LandingLink name='Security' href='#' />
+        <LandingLink name='Get Started' href='#' />
+      </div>
+      <div className='flex items-center' >
+        <button onClick={navHandler} className='text-white rounded-xl px-4 py-2 bg-blue-700 hover:bg-blue-800 transition ease-in duration-100 focus:outline-none' >Login</button>
+      </div>
+    </div >
+  )
+}
+
+const LandingLink = (props) => {
+  return (
+    <Link href={props.href} passHref >
+      <a className='p-3 text-gray-800' >
+        {props.name}
+      </a>
+    </Link>
+  )
+}
+
+
+const HomeContent = () => {
+  return (
+    <div className='w-full h-3/4 flex flex-col justify-center items-center my-auto' >
+      <h1 className='text-7xl text-blue-700 mb-9 '>Communify</h1>
+      <p className='text-lg text-center text-gray-800 mb-9' >The smartest way to farm<br />dark energy crystals.</p>
+      <div>
+        <button className=' border-2 border-blue-700 text-blue-700 hover:bg-blue-800 hover:text-white px-8 py-3 rounded-lg transition ease-in duration-100 focus:outline-none mr-4' >Learn More</button>
+        <button className='border-2 border-blue-700 bg-blue-700 text-white hover:bg-blue-800 px-8 py-3 rounded-lg transition ease-in duration-100 focus:outline-none' >Get Started</button>
+      </div>
     </div>
   )
 }
+
 
 export default HomePage
---
+++ pages/login.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
+const Login = () => {
+  const router = useRouter()
+
+  const arrowBackHandler = () => {
+    router.push('/')
+  }
+
+  const navHandler = () => {
+    router.push('/signup')
+  }
+
+  return (
+    <div className='h-screen flex justify-center items-center' >
+      <div className='flex flex-col w-96' >
+        <p className='text-sm' >&nbsp;</p>
+        <h1 className='text-center mb-4 text-3xl font-semibold' >Sign In</h1>
+        <p className='text-center mb-4 text-md' >Don't have an account? <span onClick={navHandler} className='text-blue-600 font-semibold cursor-pointer' >Sign up!</span></p>
+        <Input placeholder='email' />
+        <Input placeholder='password' />
+        <Button />
+        <p className='mt-3 text-center text-gray-800 text-sm' >&nbsp;</p>
+      </div>
+      <FontAwesomeIcon icon={faArrowLeft} onClick={arrowBackHandler} className='absolute top-0 left-0 ml-12 mt-12 text-2xl text-gray-800 cursor-pointer' />
+    </div>
+  )
+}
+
+const Input = (props) => {
+  return (
+    <input
+      type="text"
+      placeholder={props.placeholder}
+      className='w-96 h-12 pl-4 mb-4 text-gray-800 border-2 border-gray-400 rounded focus:ring-2 focus:border-blue-300 focus:ring-blue-400 focus:ring-opacity-50 focus:outline-none transition ease-in duration-100'
+    />
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button className='w-96 h-12 rounded text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-400 focus:ring-opacity-50 focus:outline-none transition ease-in duration-100' >Login</button>
+  )
+}
+
+
+export default Login
