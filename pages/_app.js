6fbc1f589e47eeb9added0c9f95d95d682ce3944
Holland Pleskac <hollandpleskac@gmail.com>
2021-05-29 11:45:46 -0700

    firebase init

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '../styles/styles.css'
+import initFirebase from '../firebase/initFirebase'
+
+initFirebase()
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
 const HomePage = () => {
+
+  const function1 = async () => {
+    console.log('adding to firebase')
+    await firebase.firestore().collection('test').add({
+      test: 'testing'
+    })
+  }
+
+
   return (
-    <div >
+    <div onClick={function1} >
       Home
     </div>
   )
